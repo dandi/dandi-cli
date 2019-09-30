44b59755b9380776a357e8ab6cd503c4f3960d45
Yaroslav Halchenko <debian@onerussian.com>
2019-09-30 13:24:29 -0400

    BF+ENH(TST): demand hdmf != 1.1.2 since causes a crash

--- dandi/tests/test_pynwb_utils.py
+++ dandi/tests/test_pynwb_utils.py
@@ -40,3 +40,11 @@ def test_get_metadata(simple1_nwb, simple1_nwb_metadata):
     target_metadata['subject_id'] = None
     assert target_metadata == get_metadata(str(simple1_nwb))
 
+
+def test_pynwb_io(simple1_nwb):
+    # To verify that our dependencies spec is sufficient to avoid
+    # stepping into known pynwb/hdmf issues
+    with pynwb.NWBHDF5IO(str(simple1_nwb), 'r', load_namespaces=True) as reader:
+        nwbfile = reader.read()
+    assert repr(nwbfile)
+    assert str(nwbfile)
