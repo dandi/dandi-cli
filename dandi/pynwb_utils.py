e46ba108281bb402e4a2ca2170f29135d4ef2573
Yaroslav Halchenko <debian@onerussian.com>
2019-09-30 13:08:27 -0400

    RF+ENH(TST), BK: get_metadata - convert h5py.Dataset to a pure list, test
    It results in the test to fail since whenever we generate (.today()) the date,
    it comes with None as tzinfo.  Upon save/reload it acquires a value, so comparison
    fails.  I am not sure yet either to trick it as a feature (and thus reflect in a test)
    or a bug in pynwb.  I will ask
    
    	> python -m pytest -s dandi/tests/test_pynwb_utils.py
    	======================================================================================= test session starts ========================================================================================
    	platform linux -- Python 3.7.3rc1, pytest-3.10.1, py-1.7.0, pluggy-0.8.0
    	hypothesis profile 'default' -> database=DirectoryBasedExampleDatabase('/home/yoh/proj/dandi/dandi-cli/.hypothesis/examples')
    	rootdir: /home/yoh/proj/dandi/dandi-cli, inifile:
    	plugins: localserver-0.5.0, cov-2.6.0, hypothesis-3.71.11
    	collected 1 item
    
    	dandi/tests/test_pynwb_utils.py F
    
    	============================================================================================= FAILURES =============================================================================================
    	________________________________________________________________________________________ test_get_metadata _________________________________________________________________________________________
    
    	simple1_nwb = '/home/yoh/.tmp/pytest-of-yoh/pytest-27/data0/simple1.nwb'
    	simple1_nwb_metadata = {'experiment_description': 'experiment_description1', 'experimenter': 'experimenter1', 'identifier': 'identifier1', 'institution': 'institution1', ...}
    
    		def test_get_metadata(simple1_nwb, simple1_nwb_metadata):
    			target_metadata = simple1_nwb_metadata.copy()
    			# we will also get some counts
    			target_metadata['number_of_electrodes'] = 0
    			target_metadata['number_of_units'] = 0
    			# subject_id is also nohow specified in that file yet
    			target_metadata['subject_id'] = None
    	>       assert target_metadata == get_metadata(str(simple1_nwb))
    	E       AssertionError: assert {'experiment_...tution1', ...} == {'experiment_d...tution1', ...}
    	E         Omitting 12 identical items, use -vv to show
    	E         Differing items:
    	E         {'session_start_time': datetime.datetime(2019, 9, 30, 13, 8, 54, 900990)} != {'session_start_time': datetime.datetime(2019, 9, 30, 13, 8, 54, 900990, tzinfo=tzoffset(None, -14400))}
    	E         Use -v to get the full diff
    
    	dandi/tests/test_pynwb_utils.py:41: AssertionError

--- dandi/pynwb_utils.py
+++ dandi/pynwb_utils.py
@@ -1,24 +1,46 @@
+import h5py
 from pynwb import NWBHDF5IO
 
-metadata_list = ('related_publications', 'lab', 'experimenter', 'keywords', 'session_start_time',
-                 'institution', 'session_description', 'session_id', 'experiment_description')
+# A list of metadata fields which dandi extracts from .nwb files.
+# Additional fields (such as `number_of_*`) might be added by the
+# get_metadata`
+metadata_fields = (
+    'experiment_description',
+    'experimenter',
+    'identifier',  # note: required arg2 of NWBFile
+    'institution',
+    'keywords',
+    'lab',
+    'related_publications',
+    'session_description',  # note: required arg1 of NWBFile
+    'session_id',
+    'session_start_time',
+)
 
 
 def get_metadata(filepath):
+    """Get selected metadata from a .nwb file
+
+    Returns
+    -------
+    dict
+    """
     out = dict()
     with NWBHDF5IO(filepath, 'r') as io:
         nwb = io.read()
-        for key in metadata_list:
-            out[key] = getattr(nwb, key)
-        out['subject_id'] = nwb.subject.subject_id
-        if hasattr(nwb, 'electrodes') and nwb.electrodes is not None:
-            out['number_of_electrodes'] = len(nwb.electrodes)
-        else:
-            out['number_of_electrodes'] = 0
+        for key in metadata_fields:
+            value = getattr(nwb, key)
+            if isinstance(value, h5py.Dataset):
+                # serialize into a basic container (list), since otherwise
+                # it would be a closed Dataset upon return
+                value = list(value)
+            out[key] = value
+        # .subject can be None as the test shows
+        out['subject_id'] = getattr(nwb.subject, 'subject_id', None)
+    # Add a few additional useful fields
 
-        if hasattr(nwb, 'units') and nwb.units is not None:
-            out['number_of_units'] = len(nwb.units)
-        else:
-            out['number_of_units'] = 0
+    # Counts
+    for f in ['electrodes', 'units']:
+        out['number_of_{}'.format(f)] = len(getattr(nwb, 'f', []) or [])
 
     return out
---
+++ dandi/tests/test_pynwb_utils.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+
+import pynwb
+
+from ..pynwb_utils import (
+    get_metadata,
+    metadata_fields,
+)
+
+import pytest
+
+
+# TODO: move into some common fixtures.  We might produce a number of files
+#       and also carry some small ones directly in git for regression testing
+@pytest.fixture(scope='session')
+def simple1_nwb_metadata(tmpdir_factory):
+    # very simple assignment with the same values as the key with 1 as suffix
+    metadata = {f: "{}1".format(f) for f in metadata_fields}
+    # tune specific ones:
+    metadata['session_start_time'] = datetime.today()
+    metadata['keywords'] = ['keyword1', 'keyword 2']
+    return metadata
+
+
+@pytest.fixture(scope='session')
+def simple1_nwb(simple1_nwb_metadata, tmpdir_factory):
+    filename = str(tmpdir_factory.mktemp('data').join('simple1.nwb'))
+    nwbfile = pynwb.NWBFile(**simple1_nwb_metadata)
+    with pynwb.NWBHDF5IO(filename, 'w') as io:
+        io.write(nwbfile, cache_spec=False)
+    return filename
+
+
+def test_get_metadata(simple1_nwb, simple1_nwb_metadata):
+    target_metadata = simple1_nwb_metadata.copy()
+    # we will also get some counts
+    target_metadata['number_of_electrodes'] = 0
+    target_metadata['number_of_units'] = 0
+    # subject_id is also nohow specified in that file yet
+    target_metadata['subject_id'] = None
+    assert target_metadata == get_metadata(str(simple1_nwb))
+
