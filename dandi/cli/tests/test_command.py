448e59ab270bfc9f7328b96af30c65acd656e521
Yaroslav Halchenko <debian@onerussian.com>
2019-10-09 20:39:29 -0400

    ENH(TST): for preceding commit

--- dandi/cli/tests/test_command.py
+++ dandi/cli/tests/test_command.py
@@ -1,3 +1,6 @@
+import sys
+from subprocess import Popen, PIPE
+
 from ..command import ls, validate
 
 from click.testing import CliRunner
@@ -14,3 +17,28 @@ def test_smoke(simple1_nwb, command):
     # empty invocation should not crash
     result = runner.invoke(command, [])
     assert result.exit_code == 0
+
+
+def test_no_heavy_imports():
+    # Timing --version for being fast is unreliable, so we will verify that
+    # no h5py or numpy (just in case) module is imported upon import of the
+    # command
+    heavy_modules = {"pynwb", "h5py", "numpy"}
+    p = Popen(
+        [
+            sys.executable,
+            "-c",
+            "import sys; "
+            "import dandi.cli.command; "
+            "print(','.join(set(m.split('.')[0] for m in sys.modules)));",
+        ],
+        stdout=PIPE,
+        stderr=PIPE,
+    )
+    stdout, stderr = p.communicate()
+    modules = stdout.decode().split(",")
+    loaded_heavy = set(modules).intersection(heavy_modules)
+
+    assert not loaded_heavy
+    assert not stderr
+    assert not p.wait()
