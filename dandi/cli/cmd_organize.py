7873f3833af9c703f31e2d55708dffe08acd0764
Yaroslav Halchenko <debian@onerussian.com>
2020-02-21 12:20:58 -0500

    ENH: populate session_id (if absent) from time, --mode, etc

--- dandi/cli/cmd_organize.py
+++ dandi/cli/cmd_organize.py
@@ -1,4 +1,9 @@
 import click
+import os
+import os.path as op
+
+from collections import Counter
+
 from .command import main, lgr
 
 
@@ -6,9 +11,10 @@ from .command import main, lgr
 @click.option(
     "-t",
     "--local-top-path",
-    help="Top directory (local) of the dataset.  Files will be uploaded with "
-    "paths relative to that directory",
+    help="Top directory (local) of the dataset.  If not specified, current "
+    "directory is assumed.",
     type=click.Path(exists=True, dir_okay=True, file_okay=False),
+    default=os.curdir,
 )
 @click.option(
     "-f",
@@ -24,13 +30,15 @@ from .command import main, lgr
     default="fail",
 )
 @click.option(
-    "--dry-run",
-    help="Just print proposed layout without moving any files",
-    default=False,
-    is_flag=True,
+    "--mode",
+    help="If 'dry' - no action is performed, suggested renames are printed. "
+    "I 'simulate' - hierarchy of empty files at --local-top-path is created. "
+    "Note that previous files should be removed prior this operation.",
+    type=click.Choice(["act", "dry", "simulate"]),
+    default="act",
 )
 @click.argument("paths", nargs=-1, type=click.Path(exists=True))
-def organize(paths, local_top_path=None, format=None, invalid="fail", dry_run=False):
+def organize(paths, local_top_path=os.curdir, format=None, invalid="fail", mode="act"):
     """(Re)organize (and rename) files according to the metadata.
 
     The purpose of this command is to provide datasets with consistently named files,
@@ -48,9 +56,11 @@ def organize(paths, local_top_path=None, format=None, invalid="fail", dry_run=Fa
         filter_invalid_metadata_rows,
     )
 
+    if mode not in ("act",):
+        raise NotImplementedError(mode)
+
     if len(paths) == 1 and paths[0].endswith(".json"):
         # Our dumps of metadata
-        dry_run = True
         metadata = load_jsonl(paths[0])
     else:
         paths = list(find_files("\.nwb$", paths=paths))
@@ -79,6 +89,47 @@ def organize(paths, local_top_path=None, format=None, invalid="fail", dry_run=Fa
             raise ValueError(f"invalid has an invalid value {invalid}")
 
     metadata = create_unique_filenames_from_metadata(metadata)
+    # Verify that we got unique paths
+    all_paths = [m["dandi_path"] for m in metadata]
+    all_paths_unique = set(all_paths)
+    if not len(all_paths) == len(all_paths_unique):
+        counts = Counter(all_paths)
+        non_unique = {p: c for p, c in counts.items() if c > 1}
+        # Let's prepare informative listing
+        nu_list = []
+        for p in non_unique:
+            orig_paths = []
+            for m in metadata:
+                if m["dandi_path"] == p:
+                    orig_paths.append(m["path"])
+            nu_list.append("    %s: %s" % (p, ", ".join(orig_paths)))
+        raise AssertionError(
+            "%d out of %d paths are not unique:\n%s"
+            % (len(non_unique), len(all_paths), "\n".join(nu_list))
+        )
+
+    # Verify first that the target paths do not exist yet, and fail if they do
+    existing = []
+    for e in metadata:
+        dandi_path = op.join(local_top_path, e["dandi_path"])
+        if op.exists(dandi_path):
+            existing.append(dandi_path)
+    if existing:
+        raise AssertionError(
+            "%d paths already exists: %s%s.  Remove them first"(
+                len(existing),
+                ", ".join(existing[:5]),
+                " and more" if len(existing) > 5 else "",
+            )
+        )
 
     for e in metadata:
+        dandi_path = op.join(local_top_path, e["dandi_path"])
         print(f"{e['path']} -> {e['dandi_path']}")
+        if mode == "dry":
+            pass  # print(f"{e['path']} -> {e['dandi_path']}")
+        elif mode == "organize":
+            with open(dandi_path, "w"):
+                pass
+        else:
+            raise NotImplementedError(mode)
--- dandi/organize.py
+++ dandi/organize.py
@@ -2,6 +2,7 @@
 ATM primarily a sandbox for some functionality for  dandi organize
 """
 
+import dateutil.parser
 import os.path as op
 
 from . import get_logger
@@ -22,13 +23,14 @@ def filter_invalid_metadata_rows(metadata_rows):
 
 
 def create_unique_filenames_from_metadata(
-    metadata_rows, mandatory=["modalities", "extension"]
+    metadata, mandatory=["modalities", "extension"]
 ):
     """
 
     Parameters
     ----------
-    metadata_rows
+    metadata: list of dict
+      List of metadata records
     mandatory: list of str
       Fields in addition to "subject_id" and (file) "extension" which would be
       mandatory to be included in the filename
@@ -56,14 +58,51 @@ def create_unique_filenames_from_metadata(
     }
     dandi_path = "sub-{subject_id}/{dandi_filename}"
 
-    # extract extensions and place them into the records
-    for r in metadata_rows:
+    #
+    # Additional fields
+    #
+
+    # extract File name extension and place them into the records
+    for r in metadata:
         r["extension"] = op.splitext(r["path"])[1]
 
     # Add "modalities" composed from the ones we could deduce
+    _populate_modalities(metadata)
+
+    # handle cases where session_id was not provided
+    # In some of those we could have session_start_time, so we could produce
+    # session_id based on those
+    if not all(m.get("session_id") for m in metadata):
+        _populate_session_ids_from_time(metadata)
+
+    unique_values = {}
+    for field in potential_fields:
+        unique_values[field] = set(r.get(field, None) for r in metadata)
+    import pdb
+
+    pdb.set_trace()
+    # unless it is mandatory, we would not include the fields with more than
+    # a single unique field
+    for r in metadata:
+        dandi_filename = ""
+        for field, field_format in potential_fields.items():
+            if field in mandatory or len(unique_values[field]) > 1:
+                value = r.get(field, None)
+                if value is not None:
+                    if isinstance(value, (list, tuple)):
+                        value = "+".join(value)
+                    formatted_value = field_format.format(value)
+                    dandi_filename += formatted_value
+        r["dandi_filename"] = dandi_filename
+        r["dandi_path"] = dandi_path.format(**r)
+
+    return metadata
+
+
+def _populate_modalities(metadata):
     ndtypes_to_modalities = get_neurodata_types_to_modalities_map()
     ndtypes_unassigned = set()
-    for r in metadata_rows:
+    for r in metadata:
         mods = set()
         for nd_rec in r.get("nd_types", "").split(","):
             # split away the count
@@ -78,54 +117,40 @@ def create_unique_filenames_from_metadata(
         # tuple so we could easier figure out "unique" values below
         r["modalities"] = tuple(sorted(mods))
 
-    unique_values = {}
-    for field in potential_fields:
-        unique_values[field] = set(r.get(field, None) for r in metadata_rows)
 
-    # unless it is mandatory, we would not include the fields with more than
-    # a single unique field
-    for r in metadata_rows:
-        dandi_filename = ""
-        for field, field_format in potential_fields.items():
-            if field in mandatory or len(unique_values[field]) > 1:
-                value = r.get(field, None)
-                if value is not None:
-                    if isinstance(value, (list, tuple)):
-                        value = "+".join(value)
-                    formatted_value = field_format.format(value)
-                    dandi_filename += formatted_value
-        r["dandi_filename"] = dandi_filename
-        r["dandi_path"] = dandi_path.format(**r)
-
-    return metadata_rows
-    # Only include fields with more than one value, for disambiguation
-    # TODO this doesn't include the fact that somes might have a value and some
-    # won't, and that would be disambiguating, but would require changes to how
-    # the filename is constructed.
-    disambiguating_fields = []
-    for field in potential_fields:
-        if len(unique_values[field]) > 1:
-            disambiguating_fields.append(field)
-
-    def construct_filename(metadata_row):
-        filename = None
-        for field in disambiguating_fields:
-            if filename:
-                filename += "_"
-            else:
-                filename = ""
-            filename += field + "_" + metadata_row[field]
-        return filename
-
-    unique_filenames = set()
-    for row in metadata_rows:
-        dandi_filename = construct_filename(row)
-        if dandi_filename in unique_filenames:
-            # Abort because we do not have enough info for uniqueness
-            raise RuntimeError("dandi_filename %s is NOT unique" % dandi_filename)
-        else:
-            row["dandi_filename"] = dandi_filename
-
-    lgr.debug("disambiguating fields: %s", disambiguating_fields)
-
-    return metadata_rows
+def _populate_session_ids_from_time(metadata):
+    ses_times = [m.get("session_start_time", None) for m in metadata]
+    if not all(ses_times):
+        lgr.warning("No session_id and no session_start_time for all samples.")
+    # Let's check if we could strip away the 0 times
+    # In ls output json we have
+    #  e.g. "2015-02-12 00:00:00+00:00"
+    # while nwb-schema says it should be ISO8601
+    #  e.g. "2018-09-28T14:43:54.123+02:00"
+    # So in the first pass just figure out if degenerate times
+    degenerate_time = True
+    for m in metadata:
+        ses_time = m.get("session_start_time", None)
+        if not ses_time:
+            continue  # we can do nothing
+        ses_time = dateutil.parser.parse(ses_time)
+        if (ses_time.hour, ses_time.minute, ses_time.second) != (0, 0, 0):
+            degenerate_time = False
+            break
+    nassigned = 0
+    for m in metadata:
+        if m.get("session_id", None):
+            continue  # this one has it
+        ses_time = m.get("session_start_time", None)
+        if not ses_time:
+            continue  # we can do nothing
+        ses_time = dateutil.parser.parse(ses_time)
+        m["session_id"] = "%d%02d%02d" % (ses_time.year, ses_time.month, ses_time.day)
+        if not degenerate_time:
+            m["session_id"] += "T%02d%02d%02d" % (
+                ses_time.hour,
+                ses_time.minute,
+                ses_time.second,
+            )
+        nassigned += 1
+    lgr.debug("Assigned %d session_id's based on the date" % nassigned)
