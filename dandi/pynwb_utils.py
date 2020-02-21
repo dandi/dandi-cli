74a203a6c56702a69024b5c5664274ecc629d051
Yaroslav Halchenko <debian@onerussian.com>
2020-02-20 22:42:19 -0500

    ENH: protect against infinite loop with attempted imports, add top level module for ndx-labmetadata-abf

--- dandi/pynwb_utils.py
+++ dandi/pynwb_utils.py
@@ -86,7 +86,11 @@ def get_metadata(path):
     # Not yet sure if PyNWB is going to provide "native" support for needed
     # functionality: https://github.com/NeurodataWithoutBorders/pynwb/issues/1143
     # So meanwhile, hard-coded workaround for data types we care about
-    ndtypes_registry = {"AIBS_ecephys": "allensdk.brain_observatory.ecephys.nwb"}
+    ndtypes_registry = {
+        "AIBS_ecephys": "allensdk.brain_observatory.ecephys.nwb",
+        "ndx-labmetadata-abf": "ndx_dandi_icephys",
+    }
+    tried_imports = set()
     while True:
         try:
             out.update(_get_pynwb_metadata(path))
@@ -104,10 +108,15 @@ def get_metadata(path):
                     "We do not know which extension provides %s. "
                     "Original exception was: %s. " % (ndtype, exc)
                 )
-            lgr.debug(
-                "Importing %r which should provide %r", ndtypes_registry[ndtype], ndtype
-            )
-            __import__(ndtypes_registry[ndtype])
+            import_mod = ndtypes_registry[ndtype]
+            lgr.debug("Importing %r which should provide %r", import_mod, ndtype)
+            if import_mod in tried_imports:
+                raise RuntimeError(
+                    "We already tried importing %s to provide %s, but it seems it didn't help"
+                    % (import_mod, ndtype)
+                )
+            tried_imports.add(import_mod)
+            __import__(import_mod)
 
     return out
 
