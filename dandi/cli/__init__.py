2d5d57a6b500666b1ac510f3c56905ae963cb300
Yaroslav Halchenko <debian@onerussian.com>
2020-02-21 22:50:08 -0500

    Avoid MPI initialization in mpi4py to avoid lengthy imports ATM

--- dandi/cli/__init__.py
+++ dandi/cli/__init__.py
@@ -7,3 +7,13 @@ TODO:
   too cubmersome.  yoh thinks he saw a bit more lightweight somewhere.
   e.g. girder-client
 """
+
+try:
+    # A trick found on https://github.com/h5py/h5py/issues/1079#issuecomment-567081386
+    # to avoid some weird behavior on Yarik's laptop where MPI fails to initialize
+    # and that takes h5py additional 5 seconds to import
+    import mpi4py
+
+    mpi4py.rc(initialize=False)
+except:
+    pass
