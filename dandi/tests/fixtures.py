9bdb86035fa02e0a2ffa6ea9320c2c2ccfdbdc08
Yaroslav Halchenko <debian@onerussian.com>
2020-03-09 17:49:39 -0400

    ENH(TST): get_gitrepo_fixture() and nwb_test_data for testing

--- dandi/tests/fixtures.py
+++ dandi/tests/fixtures.py
@@ -1,10 +1,18 @@
 from datetime import datetime
 from dateutil.tz import tzutc
+from subprocess import run
+import shutil
+import tempfile
 
 import pynwb
 from ..pynwb_utils import metadata_fields
 
 import pytest
+
+from .. import get_logger
+
+
+lgr = get_logger()
 
 # TODO: move into some common fixtures.  We might produce a number of files
 #       and also carry some small ones directly in git for regression testing
@@ -33,3 +41,40 @@ def simple1_nwb(simple1_nwb_metadata, tmpdir_factory):
     with pynwb.NWBHDF5IO(filename, "w") as io:
         io.write(nwbfile, cache_spec=False)
     return filename
+
+
+@pytest.fixture()
+def clirunner():
+    """A shortcut to get a click.runner to run a command"""
+    from click.testing import CliRunner
+
+    yield CliRunner()
+
+
+def get_gitrepo_fixture(url, commitish=None, scope="session"):
+
+    if commitish:
+        raise NotImplementedError()
+
+    @pytest.fixture(scope=scope)
+    def fixture():
+        # TODO: adapt reproman.tests.skip collection of skipif conditions
+        # skipif.no_network()
+        # skipif.no_git()
+
+        path = tempfile.mktemp()  # not using pytest's tmpdir fixture to not
+        # collide in different scopes etc. But we
+        # would need to remove it ourselves
+        lgr.debug("Cloning %r into %r", url, path)
+        try:
+            runout = run(["git", "clone", url, path])
+            if runout.returncode:
+                raise RuntimeError(f"Failed to clone {url} into {path}")
+            yield path
+        finally:
+            shutil.rmtree(path)
+
+    return fixture
+
+
+nwb_test_data = get_gitrepo_fixture("http://github.com/dandi-datasets/nwb_test_data")
