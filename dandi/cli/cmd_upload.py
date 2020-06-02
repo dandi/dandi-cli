3c983b04d9854342c902e58b6fdd6d0e0d713d48
Yaroslav Halchenko <debian@onerussian.com>
2020-06-02 18:21:32 -0400

    ENH: Include "uploaded_digests" with md5, sha1, sha256 into metadata upon upload
    If for some reason computing digests fails - upload of that file will be skipped.
    Crude but better be "safe than sorry" -- we are using only standard library here
    so there should be no failures unless permissions etc forbid access to the file

--- dandi/cli/cmd_upload.py
+++ dandi/cli/cmd_upload.py
@@ -97,6 +97,7 @@ def upload(
     """
     from pathlib import Path, PurePosixPath
     from ..dandiset import Dandiset
+    from ..support.digests import Digester
 
     dandiset = Dandiset.find(dandiset_path)
     if not dandiset:
@@ -407,6 +408,20 @@ def upload(
                         client.delete(f'/item/{item_rec["_id"]}')
                         yield {"status", "deleted empty item"}
                     return
+
+            #
+            # ?. Compute checksums and possible other digests (e.g. for s3, ipfs - TODO)
+            #
+            yield {"status": "digesting"}
+            try:
+                # TODO: in theory we could also cache the result, but since it is
+                # critical to get correct checksums, safer to just do it all the time.
+                # Should typically be faster than upload itself ;-)
+                digester = Digester(["md5", "sha1", "sha256"])
+                file_metadata_["uploaded_digests"] = digester(path)
+            except Exception as exc:
+                yield skip_file("failed to compute digests: %s" % str(exc))
+                return
 
             #
             # 5. Upload file
