61c0199a8bf73edbd853e0f19cf0f546ea1798c2
John T. Wodder II <git@varonathe.org>
2020-08-03 10:16:11 -0400

    List repository URL in project_urls
    download_url (which seems to be effectively deprecated anyway) is not the
    appropriate place to put a repository URL.
    
    This requires setuptools 38.3.0+.

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ except ImportError:
 # Give setuptools a hint to complain if it's too old a version
 # 30.3.0 allows us to put most metadata in setup.cfg
 # Should match pyproject.toml
-SETUP_REQUIRES = ["setuptools >= 30.3.0"]
+SETUP_REQUIRES = ["setuptools >= 38.3.0"]
 # This enables setuptools to install wheel on-the-fly
 SETUP_REQUIRES += ["wheel"] if "bdist_wheel" in sys.argv else []
 
