c9d3cf26f724c1b2f685d89d4f2b45f63754b004
Yaroslav Halchenko <debian@onerussian.com>
2020-08-07 10:44:12 -0400

    ENH(TST): make sleeps shorter, but keep last outter sleep long
    With the hope that it would make test faster and also avoid sporadic
    test failures on some runs on CI where it does manage to get to the 2nd
    element before exception is raised:
    
        >       assert got == [0]
        E       assert [0, 1] == [0]
        E         Left contains one more item: 1
        E         Full diff:
        E         - [0]
        E         + [0, 1]

--- dandi/support/tests/test_iterators.py
+++ dandi/support/tests/test_iterators.py
@@ -17,27 +17,27 @@ def sleeping_range(n, secs=0.01, thr=None):
 def test_IteratorWithAggregation():
     sumup = lambda v, t=0: v + t
 
-    it = IteratorWithAggregation(sleeping_range(3, 0.001), agg=sumup)
+    it = IteratorWithAggregation(sleeping_range(3, 0.0001), agg=sumup)
     # we should get our summary available after 2nd iteration and before it finishes
     for t, i in enumerate(it):
-        sleep(0.003)
+        sleep(0.0003)
         assert t == i  # it is just a range after all
         if i:
             assert it.finished
 
     # If there is an exception thrown, it would be raised only by the end
-    it = IteratorWithAggregation(sleeping_range(5, 0.001, thr=2), agg=sumup)
+    it = IteratorWithAggregation(sleeping_range(5, 0.0001, thr=2), agg=sumup)
     got = []
     with pytest.raises(ValueError):
         for i in it:
             got.append(i)
-            sleep(0.003)
+            sleep(0.0003)
     assert got == [0, 1, 2]
     assert it.finished == True
 
     # If there is an exception thrown, it would be raised only by the end
     it = IteratorWithAggregation(
-        sleeping_range(5, 0.001, thr=2), agg=sumup, reraise_immediately=True
+        sleeping_range(5, 0.0001, thr=2), agg=sumup, reraise_immediately=True
     )
     got = []
     with pytest.raises(ValueError):
