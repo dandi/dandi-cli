8dc0d1767a03e8f16a8464ef5c8bf569b539f913
John T. Wodder II <git@varonathe.org>
2021-03-17 14:53:56 -0400

    Some tests

---
+++ dandi/core/digests/tests/test_dandietag.py
@@ -0,0 +1,20 @@
+import pytest
+
+from ..dandietag import DANDIEtag, mb, tb
+
+
+@pytest.mark.parametrize(
+    "file_size,initial_part_size,final_part_size,part_count",
+    [
+        (mb(64), mb(64), mb(64), 1),
+        (mb(50), mb(50), mb(50), 1),
+        (mb(70), mb(64), mb(6), 2),
+        (mb(140), mb(64), mb(12), 3),
+        (tb(5), 549755814, 549754694, 10000),
+    ],
+)
+def test_gen_part_sizes(file_size, initial_part_size, final_part_size, part_count):
+    sizes = DANDIEtag.gen_part_sizes(file_size)
+    assert len(sizes) == part_count
+    assert all(sz == initial_part_size for sz in sizes[:-1])
+    assert sizes[-1] == final_part_size
