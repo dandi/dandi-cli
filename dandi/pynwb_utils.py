bfe168c6143e7de27d633c481059df6173297e9c
John T. Wodder II <git@varonathe.org>
2021-03-24 09:00:14 -0400

    Add a check for null NWB versions

--- dandi/pynwb_utils.py
+++ dandi/pynwb_utils.py
@@ -261,19 +261,20 @@ def validate(path, devel_debug=False):
         # we just will not remove any errors, it is required so should be some
         pass
     else:
-        # Explicitly sanitize so we collect warnings.
-        # TODO: later cast into proper ERRORs
-        version = _sanitize_nwb_version(version, log=errors.append)
-        loosever = LooseVersion(version)
-        if loosever and loosever < "2.1.0":
-            errors_ = errors[:]
-            errors = [e for e in errors if not re_ok_prior_210.search(str(e))]
-            if errors != errors_:
-                lgr.debug(
-                    "Filtered out %d validation errors on %s",
-                    len(errors_) - len(errors),
-                    path,
-                )
+        if version is not None:
+            # Explicitly sanitize so we collect warnings.
+            # TODO: later cast into proper ERRORs
+            version = _sanitize_nwb_version(version, log=errors.append)
+            loosever = LooseVersion(version)
+            if loosever and loosever < "2.1.0":
+                errors_ = errors[:]
+                errors = [e for e in errors if not re_ok_prior_210.search(str(e))]
+                if errors != errors_:
+                    lgr.debug(
+                        "Filtered out %d validation errors on %s",
+                        len(errors_) - len(errors),
+                        path,
+                    )
     return errors
 
 
