74f4a5f29dd60964c58b031b825cb7e5874c3886
Satrajit Ghosh <satra@mit.edu>
2021-05-10 17:02:58 -0400

    fix: validation yaml code

--- dandi/validate.py
+++ dandi/validate.py
@@ -66,7 +66,7 @@ def validate_dandiset_yaml(filepath, schema_version=None, devel_debug=False):
                 f"Unsupported schema version: {schema_version}; expected {current_version}"
             )
         try:
-            DandisetMeta(**meta.dict())
+            DandisetMeta(**meta)
         except ValidationError as e:
             if devel_debug:
                 raise
@@ -83,7 +83,7 @@ def validate_dandiset_yaml(filepath, schema_version=None, devel_debug=False):
                 e,
                 extra={"validating": True},
             )
-            return [f"Failed to convert metadata: {e}"]
+            return [f"Failed to initialize Dandiset meta: {e}"]
         return []
 
 
