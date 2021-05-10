4fc934c93acb0ea6c7a063a6a4bd837015dff601
Satrajit Ghosh <satra@mit.edu>
2021-05-10 13:01:59 -0400

    fix: remove more traces of migrate2newschema

--- dandi/validate.py
+++ dandi/validate.py
@@ -58,7 +58,6 @@ def validate_dandiset_yaml(filepath, schema_version=None, devel_debug=False):
     else:
         from pydantic import ValidationError
 
-        from .metadata import migrate2newschema
         from .models import DandisetMeta, get_schema_version
 
         current_version = get_schema_version()
@@ -67,8 +66,7 @@ def validate_dandiset_yaml(filepath, schema_version=None, devel_debug=False):
                 f"Unsupported schema version: {schema_version}; expected {current_version}"
             )
         try:
-            new_meta = migrate2newschema(meta)
-            DandisetMeta(**new_meta.dict())
+            DandisetMeta(**meta.dict())
         except ValidationError as e:
             if devel_debug:
                 raise
