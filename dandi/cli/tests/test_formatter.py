64e6d5395a032eb68ab61633a8971d0ca9370094
John T. Wodder II <git@varonathe.org>
2021-06-02 14:19:31 -0400

    Test JSONLinesFormatter

--- dandi/cli/tests/test_formatter.py
+++ dandi/cli/tests/test_formatter.py
@@ -2,7 +2,7 @@ from io import StringIO
 
 import pytest
 
-from ..formatter import JSONFormatter
+from ..formatter import JSONFormatter, JSONLinesFormatter
 
 
 def test_json_formatter():
@@ -46,3 +46,22 @@ def test_json_formatter_empty(indent):
     with fmtr:
         pass
     assert out.getvalue() == "[]\n"
+
+
+def test_json_lines_formatter():
+    out = StringIO()
+    fmtr = JSONLinesFormatter(out=out)
+    with fmtr:
+        fmtr({"foo": 23, "bar": 42})
+        fmtr({"bar": "gnusto", "foo": "cleesh"})
+    assert out.getvalue() == (
+        '{"bar": 42, "foo": 23}\n{"bar": "gnusto", "foo": "cleesh"}\n'
+    )
+
+
+def test_json_lines_formatter_empty():
+    out = StringIO()
+    fmtr = JSONLinesFormatter(out=out)
+    with fmtr:
+        pass
+    assert out.getvalue() == ""
