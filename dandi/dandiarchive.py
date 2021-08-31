780123a71854b533c32f63cf2f0aaacd2b5e49c9
John T. Wodder II <jwodder@users.noreply.github.com>
2021-08-31 17:01:13 -0400

    404 → 400
    Co-authored-by: Yaroslav Halchenko <debian@onerussian.com>

--- dandi/dandiarchive.py
+++ dandi/dandiarchive.py
@@ -526,7 +526,7 @@ class _dandi_url_parser:
         i = 0
         while True:
             r = requests.head(url, allow_redirects=True)
-            if r.status_code in (404, 502, 503, 504):
+            if r.status_code in (400, 502, 503, 504):
                 if i < 5:
                     sleep(0.1 * 10 ** i)
                     i += 1
