28ba095362c1851be6fdc17d42a8cbebeca59431
John T. Wodder II <git@varonathe.org>
2021-10-06 11:10:24 -0400

    Enable viewcode extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,12 @@ release = dandi.__version__
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.autosummary", "sphinx.ext.napoleon"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
