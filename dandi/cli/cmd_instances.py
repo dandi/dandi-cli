696c3eb1eaef1ab04930e153abbeaa2c80f2835e
John T. Wodder II <git@varonathe.org>
2021-10-15 15:24:47 -0400

    Fix for Python 3.7

--- dandi/cli/cmd_instances.py
+++ dandi/cli/cmd_instances.py
@@ -13,4 +13,6 @@ def instances():
     """List known Dandi Archive instances that the CLI can interact with"""
     yaml = ruamel.yaml.YAML(typ="safe")
     yaml.default_flow_style = False
-    yaml.dump({k: v._asdict() for k, v in known_instances.items()}, sys.stdout)
+    # Convert _asdict() with dict() so that ruamel doesn't tag the results with
+    # "!!omap" on Python 3.7
+    yaml.dump({k: dict(v._asdict()) for k, v in known_instances.items()}, sys.stdout)
