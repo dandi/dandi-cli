f0564f3021971e9e58586beb5744462b201ca31c
Horea Christian <chr@chymera.eu>
2022-03-20 20:52:10 -0400

    Validation integration test

--- dandi/tests/test_bids_validator_xs.py
+++ dandi/tests/test_bids_validator_xs.py
@@ -288,3 +288,35 @@ def test_write_report():
     with open(expected_report_path, "r") as f:
         expected_report_text = f.read()
     assert report_text == expected_report_text
+
+
+def test_bids_datasets(bids_examples: str):
+    from dandi.bids_validator_xs import validate_bids
+
+    whitelist = [
+        "qmri_megre",
+        "asl003",
+        "pet002",
+        "asl005",
+        "asl002",
+        "pet004",
+        "eeg_cbm",
+        "asl001",
+        "pet005",
+        "hcp_example_bids",
+        "asl004",
+        "qmri_tb1tfl",
+        "micr_SPIM",
+        "pet001",
+        "pet003",
+        "micr_SEM",
+    ]
+    schema_path = "{module_path}/support/bids/schemadata/1.7.0+012+dandi001"
+
+    for i in os.listdir(bids_examples):
+        if i in whitelist:
+            j = validate_bids(
+                os.path.join(bids_examples, i), schema_version=schema_path
+            )
+            # Have all files been validated?
+            assert len(j["path_tracking"]) == 0
