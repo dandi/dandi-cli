a9370c9635f04244eb362e7944884e31b7e867e4
Yaroslav Halchenko <debian@onerussian.com>
2022-05-05 10:27:58 -0400

    Make force a posarg at click CLI before truly optional devel_debug
    Co-authored-by: John T. Wodder II <jwodder@users.noreply.github.com>

--- dandi/cli/cmd_delete.py
+++ dandi/cli/cmd_delete.py
@@ -14,7 +14,7 @@ from .base import devel_debug_option, instance_option, map_to_click_exceptions
 @instance_option()
 @devel_debug_option()
 @map_to_click_exceptions
-def delete(paths, skip_missing, dandi_instance, devel_debug=False, force=False):
+def delete(paths, skip_missing, dandi_instance, force, devel_debug=False):
     """Delete dandisets and assets from the server.
 
     PATH could be a local path or a URL to an asset, directory, or an entire
