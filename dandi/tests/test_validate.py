c367e052a05ef5ff935b9b334a4e282420cf3e91
Horea Christian <chr@chymera.eu>
2022-07-19 18:57:28 -0400

    verify report file creation

---
+++ dandi/tests/test_validate.py
@@ -0,0 +1,15 @@
+from glob import glob
+import os
+
+import appdirs
+
+
+def test_validate_bids(bids_examples):
+    from ..validate import validate_bids
+
+    selected_dataset = os.path.join(bids_examples, os.listdir(bids_examples)[0])
+    _ = validate_bids(selected_dataset, report=True)
+    # Check if a report is being produced.
+    pid = os.getpid()
+    log_dir = appdirs.user_log_dir("dandi-cli", "dandi")
+    assert len(glob(f"{log_dir}/bids-validator-report_*-{pid}.log")) == 1
