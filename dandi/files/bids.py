c93192309b606173e24d70aad8804a3507a02646
John T. Wodder II <git@varonathe.org>
2022-07-21 10:04:39 -0400

    Add docstrings to BIDS classes

--- dandi/files/bids.py
+++ dandi/files/bids.py
@@ -10,26 +10,50 @@ from .zarr import ZarrAsset
 
 @dataclass
 class BIDSDatasetDescriptionAsset(LocalFileAsset):
+    """
+    The :file:`dataset_description.json` file for a BIDS dataset, used to
+    perform operations on the dataset as a whole
+    """
+
+    #: A list of all other assets in the dataset
     dataset_files: list[BIDSAsset] = field(default_factory=list)
 
     @property
     def bids_root(self) -> Path:
+        """
+        The directory on the filesystem in which the BIDS dataset is located
+        """
         return self.filepath.parent
 
 
 @dataclass
 class BIDSAsset(LocalFileAsset):
+    """
+    Base class for non-:file:`dataset_description.json` assets in BIDS datasets
+    """
+
+    #: A weak reference to the :file:`dataset_description.json` file for the
+    #: containing dataset.
+    #:
+    #: Users are advised to use `bids_dataset_description` to access the
+    #: :file:`dataset_description.json` file instead.
     bids_dataset_description_ref: weakref.ref[BIDSDatasetDescriptionAsset]
 
     @property
     def bids_dataset_description(self) -> BIDSDatasetDescriptionAsset:
+        """
+        The :file:`dataset_description.json` file for the containing dataset
+        """
         bdd = self.bids_dataset_description_ref()
         assert bdd is not None
         return bdd
 
     @property
     def bids_root(self) -> Path:
-        return self.bids_dataset_description.filepath.parent
+        """
+        The directory on the filesystem in which the BIDS dataset is located
+        """
+        return self.bids_dataset_description.bids_root
 
     @property
     def bids_path(self) -> str:
@@ -40,12 +64,22 @@ class BIDSAsset(LocalFileAsset):
 
 
 class NWBBIDSAsset(BIDSAsset, NWBAsset):
+    """An NWB file in a BIDS dataset"""
+
     pass
 
 
 class ZarrBIDSAsset(BIDSAsset, ZarrAsset):
+    """A Zarr directory in a BIDS dataset"""
+
     pass
 
 
 class GenericBIDSAsset(BIDSAsset, GenericAsset):
+    """
+    An asset in a BIDS dataset that is not an NWB file, a Zarr directory, or a
+    :file:`dataset_description.json` file.  Note that, unlike the non-BIDS
+    classes, this includes video files.
+    """
+
     pass
