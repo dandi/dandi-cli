02a6e95d8ebb34b369aab19c525d16508d269d34
Horea Christian <chr@chymera.eu>
2022-07-22 04:32:22 -0400

    Using pytest's tmp_path

--- dandi/tests/test_validate.py
+++ dandi/tests/test_validate.py
@@ -4,7 +4,7 @@ import os
 import appdirs
 
 
-def test_validate_bids(bids_examples):
+def test_validate_bids(bids_examples, tmp_path):
     from ..validate import validate_bids
 
     # TEST DEFAULT REPORT PATH:
@@ -24,10 +24,12 @@ def test_validate_bids(bids_examples):
     # Replace explicit dataset with `os.listdir(bids_examples)[1]` whenever we
     # implement light data cloning analogous to (`[1]` because `[0]` is .git):
     # https://github.com/bids-standard/bids-specification/pull/1143
+    report_path = os.path.join(tmp_path, "inplace_bids-validator-report.log")
     selected_dataset = os.path.join(bids_examples, "asl003")
     _ = validate_bids(
-        selected_dataset, report_path="/tmp/inplace_bids-validator-report.log"
+        selected_dataset,
+        report_path=report_path,
     )
 
     # Check if a report is being produced.
-    assert len(glob("/tmp/inplace_bids-validator-report.log")) == 1
+    assert len(glob(report_path)) == 1
