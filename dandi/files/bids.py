4f248b966abb2b3d72f5ab352b2d7195cb3fa7e5
Horea Christian <chr@chymera.eu>
2022-09-08 16:53:08 -0400

    Typing fixes

--- dandi/files/bids.py
+++ dandi/files/bids.py
@@ -74,12 +74,14 @@ class BIDSDatasetDescriptionAsset(LocalFileAsset):
                 self._asset_metadata = defaultdict(dict)
                 for i in results:
                     if i.id in BIDS_ASSET_ERRORS:
-                        self._asset_errors[i.path].append(i.message)
+                        self._asset_errors[str(i.path)].append(i.message)
                     elif i.id in BIDS_DATASET_ERRORS:
                         self._dataset_errors.append(i.message)
                     elif i.id == "BIDS.MATCH":
                         bids_path = Path(i.path).relative_to(self.bids_root).as_posix()
-                        self._asset_metadata[bids_path] = prepare_metadata(i.metadata)
+                        self._asset_metadata[bids_path] = prepare_metadata(
+                            dict(i.metadata)
+                        )
 
     def get_asset_errors(self, asset: BIDSAsset) -> list[str]:
         """:meta private:"""
