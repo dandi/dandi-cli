72f47a0ba0266e32a6a19c3f6d57d9428ae8324b
Horea Christian <chr@chymera.eu>
2022-09-12 10:14:14 -0400

    Update error tests

--- dandi/cli/tests/test_cmd_validate.py
+++ dandi/cli/tests/test_cmd_validate.py
@@ -3,15 +3,12 @@ import os
 from click.testing import CliRunner
 
 
-def test_validate_bids_error(bids_examples):
+def test_validate_bids_error(bids_error_examples):
     from ..cmd_validate import validate_bids
 
-    expected_error = (
-        "File does not match any pattern known to BIDS.\n"
-        "BIDS-required file is not present.\n"
-    )
+    expected_error = "File does not match any pattern known to BIDS.\n"
 
-    broken_dataset = os.path.join(bids_examples, "invalid_pet001")
+    broken_dataset = os.path.join(bids_error_examples, "invalid_pet001")
     r = CliRunner().invoke(validate_bids, [broken_dataset])
     # Does it break?
     assert r.exit_code == 1
