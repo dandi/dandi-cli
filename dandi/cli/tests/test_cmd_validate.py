af48872e1babb9d2cf84eff35756f125ee416072
Horea Christian <chr@chymera.eu>
2022-10-05 16:19:15 -0400

    Removed unneeded imports, added tests for broken nwb dataset

--- dandi/cli/tests/test_cmd_validate.py
+++ dandi/cli/tests/test_cmd_validate.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 from click.testing import CliRunner
@@ -8,7 +9,6 @@ import pytest
     "dataset", ["invalid_asl003", "invalid_eeg_cbm", "invalid_pet001"]
 )
 def test_validate_bids_error(bids_error_examples, dataset):
-    import json
 
     from ..cmd_validate import validate_bids
 
@@ -24,22 +24,55 @@ def test_validate_bids_error(bids_error_examples, dataset):
         assert key in r.output
 
 
-def test_validate_bids_error_grouping(bids_error_examples, dataset="invalid_asl003"):
+def test_validate_bids_error_grouping_nwb(
+    bids_error_examples, dataset="invalid_asl003"
+):
     """
     This is currently a placeholder test, and should be updated once we have paths with
     multiple errors.
     """
-    import json
 
     from ..cmd_validate import validate_bids
 
-    broken_dataset = os.path.join(bids_error_examples, dataset)
-    with open(os.path.join(broken_dataset, ".ERRORS.json")) as f:
-        expected_errors = json.load(f)
-    r = CliRunner().invoke(validate_bids, ["--grouping=path", broken_dataset])
+    dataset = os.path.join(bids_error_examples, dataset)
+    r = CliRunner().invoke(validate_bids, ["--grouping=path", dataset])
     # Does it break?
     assert r.exit_code == 1
 
     # Does it detect all errors?
-    for key in expected_errors:
-        assert key in r.output
+    assert dataset in r.output
+
+
+def test_validate_bids_error_grouping_bids(simple3_nwb):
+    """
+    This is currently a placeholder test, and should be updated once we have paths with
+    multiple errors.
+    """
+
+    from ..cmd_validate import validate_bids
+
+    r = CliRunner().invoke(validate_bids, ["--grouping=path", simple3_nwb])
+    # Does it break?
+    assert r.exit_code == 1
+
+    # Does it detect all errors?
+    assert simple3_nwb in r.output
+
+
+def test_validate_bids_error_grouping_notification(
+    bids_error_examples, dataset="invalid_asl003"
+):
+    """Test user notification for unimplemented parameter value."""
+
+    from ..cmd_validate import validate_bids
+
+    broken_dataset = os.path.join(bids_error_examples, dataset)
+    r = CliRunner().invoke(validate_bids, ["--grouping=error", broken_dataset])
+    # Does it break?
+    assert r.exit_code == 1
+
+    # Does it notify the user correctly?
+    notification_substring = (
+        "`grouping` parameter values currently supported are path or None"
+    )
+    assert notification_substring in r.output
