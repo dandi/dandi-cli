9ae726b85de207663b1d5afa2b15dfc72f39be96
Yaroslav Halchenko <debian@onerussian.com>
2023-03-17 11:52:22 -0400

    Add an example for streaming using as_readable + reference to PyNWB examples

---
+++ docs/source/examples/dandiapi-as_readable.py
@@ -0,0 +1,14 @@
+from dandi.dandiapi import DandiAPIClient
+
+dandiset_id = "000006"  # ephys dataset from the Svoboda Lab
+filepath = "sub-anm372795/sub-anm372795_ses-20170718.nwb"  # 450 kB file
+
+with DandiAPIClient() as client:
+    asset = client.get_dandiset(dandiset_id, "draft").get_asset_by_path(filepath)
+    # https://dandi.readthedocs.io/en/latest/modref/dandiapi.html#dandi.dandiapi.BaseRemoteBlobAsset.as_readable
+    # provides File like object which via fsspec provides sparse access to content
+    # of the file on S3:
+    with asset.as_readable().open() as f:
+        print(f.read(4))
+        f.seek(100)
+        print(f.read(4))
