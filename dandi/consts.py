b4fab27568366b4c73231c04b91d1adefba54c88
Ryan Ly <rly@lbl.gov>
2023-09-11 17:00:18 -0700

    Add .h264 as a video file extension

--- dandi/consts.py
+++ dandi/consts.py
@@ -164,7 +164,7 @@ DRAFT = "draft"
 #: of retries)
 RETRY_STATUSES = (500, 502, 503, 504)
 
-VIDEO_FILE_EXTENSIONS = [".mp4", ".avi", ".wmv", ".mov", ".flv", ".mkv"]
+VIDEO_FILE_EXTENSIONS = [".mp4", ".avi", ".wmv", ".mov", ".flv", ".mkv", ".h264"]
 VIDEO_FILE_MODULES = ["processing", "acquisition"]
 
 ZARR_EXTENSIONS = [".ngff", ".zarr"]
