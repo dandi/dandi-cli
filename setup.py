78edc3625f24830d5ed47ca90cdaa3c42bfed5f4
Aaron Kanzer <aaronkanzer@Aarons-MacBook-Pro.local>
2024-07-18 13:26:16 -0400

    temp bump on local version

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ try:
     import versioneer
 
     setup_kw = {
-        "version": "0.8.5",
+        "version": "0.8.6",
         "cmdclass": versioneer.get_cmdclass(),
     }
 except ImportError:
