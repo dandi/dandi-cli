d59e7785dc951c977c2761a0b686bcaa19953ac3
Kabilar Gunalan <kabi@mit.edu>
2024-11-28 22:26:34 -0600

    Add Meriones unguiculatus to species list

--- dandi/metadata/util.py
+++ dandi/metadata/util.py
@@ -394,6 +394,12 @@ species_map = [
         NCBITAXON_URI_TEMPLATE.format("9545"),
         "Macaca nemestrina",
     ),
+    (
+        ["Mongolian gerbil", "Mongolian jird"],
+        None,
+        NCBITAXON_URI_TEMPLATE.format("10047"),
+        "Meriones unguiculatus",
+    ),
 ]
 
 
