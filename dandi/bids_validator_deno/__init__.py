29e7fd69995a6ac4bf51c94119c83b6bdc9ef21e
Isaac To <isaac.chun.to@gmail.com>
2025-04-03 23:49:40 -0700

    feat: harmonize `BidsValidationResult` into `list[ValidationResult]`

--- dandi/bids_validator_deno/__init__.py
+++ dandi/bids_validator_deno/__init__.py
@@ -2,18 +2,39 @@
 # validator, https://pypi.org/project/bids-validator-deno/.
 
 from functools import cache
+from pathlib import Path
 import re
 from subprocess import CalledProcessError, CompletedProcess, TimeoutExpired, run
+from typing import Optional
 
 from pydantic import DirectoryPath, validate_call
 
-from dandi.bids_validator_deno.models import BidsValidationResult
+from dandi.bids_validator_deno.models import BidsValidationResult, Issue
+from dandi.bids_validator_deno.models import Severity as BidsSeverity
+from dandi.utils import find_parent_directory_containing
+from dandi.validate_types import (
+    Origin,
+    OriginType,
+    Scope,
+    Severity,
+    Standard,
+    ValidationResult,
+    Validator,
+)
 
 CMD = "bids-validator-deno"
 TIMEOUT = 600.0  # 10 minutes, in seconds
 
 # ANSI SGR (Select Graphic Rendition) pattern
 _ANSI_SGR_PATTERN = re.compile(r"\x1b\[[0-9;]*m")
+
+# Map from BIDS validator severity levels to Dandi severity levels
+# Note: BidsSeverity.ignore level is not mapped. Issues with this severity level will
+#   be ignored in the harmonization process.
+_SEVERITY_MAP = {
+    BidsSeverity.warning: Severity.HINT,
+    BidsSeverity.error: Severity.ERROR,
+}
 
 
 class ValidatorError(Exception):
@@ -163,3 +184,92 @@ def get_version() -> str:
         )
 
     return version
+
+
+def harmonize(bv_result: BidsValidationResult, ds_path: Path) -> list[ValidationResult]:
+    """
+    Harmonize a `BidsValidationResult` object into a list of `ValidationResult` objects
+
+    Parameters
+    ----------
+    bv_result : BidsValidationResult
+        The `BidsValidationResult` object representing the result of the validation
+        using the deno-compiled BIDS validator
+    ds_path : Path
+        The path to the dataset that has been validated to produce the `bv_result`
+        object
+
+    Returns
+    -------
+    list[ValidationResult]
+        A list of `ValidationResult` objects in which each object represents an issue
+        in the validation result.
+    """
+    issues = bv_result.issues.issues
+    code_messages = bv_result.issues.codeMessages
+    schema_version = bv_result.summary.schemaVersion
+    dandiset_path = find_parent_directory_containing("dandiset.yaml", ds_path)
+    dataset_path = find_parent_directory_containing("dataset_description.json", ds_path)
+
+    origin = Origin(
+        type=OriginType.VALIDATION,
+        validator=Validator.bids_validator_deno,
+        validator_version=get_version(),
+        standard=Standard.BIDS,
+        # todo: the BIDSVersion is unavailable through the validator;
+        #   However, the schema version is actually more precise
+        standard_version=schema_version,
+    )
+
+    results: list[ValidationResult] = []
+    for issue in issues:
+        if issue.severity is BidsSeverity.ignore:
+            # Ignore issues with severity "ignore"
+            # TODO: If we want to include these issues, we will have to add a new value
+            #   to the Severity enum.
+            continue
+
+        results.append(
+            ValidationResult(
+                id=f"BIDS.{issue.code}",
+                origin=origin,
+                scope=Scope.FILE,  # TODO: it can be folder or dataset as well, possibly
+                origin_result=issue,  # TODO: it may be more useful if set to `bv_result`
+                severity=_SEVERITY_MAP.get(issue.severity),
+                dandiset_path=dandiset_path,
+                dataset_path=dataset_path,
+                message=_get_msg(issue, code_messages),
+                # metadata, not sure if this can be done it is there is SubjectMetadata in summary
+                # path=,
+            )
+        )
+
+    return results
+
+
+# todo: to be tested
+def _get_msg(issue: Issue, code_messages: dict[str, str]) -> Optional[str]:
+    """
+    Given an issue from the validation result of the deno-compiled BIDS validator,
+    produce a human-readable message.
+
+    Parameters
+    ----------
+    issue : Issue
+        The issue to produce a message for
+    code_messages : dict[str, str]
+        A dictionary mapping issue codes to human-readable messages given as part of
+        the validation result
+    Returns
+    -------
+    Optional[str]
+        The human-readable message (or `None` if such a message can't be produced)
+    """
+    coded_msg = code_messages.get(issue.code, "")
+    sub_code_msg = f"subCode: {issue.subCode}" if issue.subCode else ""
+    issue_msg = f"issueMessage: {issue.issueMessage}" if issue.issueMessage else ""
+
+    msg = "\n".join(filter(None, [coded_msg, sub_code_msg, issue_msg]))
+
+    # Return `None` if a non-empty message cannot be produced
+    return msg if msg else None
